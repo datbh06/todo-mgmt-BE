a724b0108679b5a12388affb0c93dc48715a14c4
datbh <bdat1606@gmail.com>
2023-08-17 23:02:49 +0700

    Initial commit

---
+++ src/test/java/com/yugen/todomgmtbe/TodoMgmtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugen.todomgmtbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMgmtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
